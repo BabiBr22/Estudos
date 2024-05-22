938167cf2401e5ad132b3d150e5bacbf66d732f9
BÁRBARA COELHO RIOS <barbara_rios@estudante.sesisenai.org.br>
2024-05-22 16:43:33 -0300

    comiti das altereções da calculadora

--- Sergio/exemplo1.js
+++ Sergio/exemplo1.js
@@ -31,39 +31,129 @@
 // -----------------------
 
 
-let botao, quebraLinha, jogada = 1;
+let tabuleiro = new Array(3)
+let botao, quebralinha, jogada = 1
 
-let tabuleiro = new Array(3);
-
-for(let i = 0; i < tabuleiro.length; i++){
-    tabuleiro[i] = new Array(3);
+for (let i = 0; i < tabuleiro.length; i++) {
+    tabuleiro[i] = new Array(3)
 }
 
-for(let i = 0; i < tabuleiro.length; i++){
-    quebraLinha = document.createElement('br');
-    document.body.append(quebraLinha);
-    for(let j = 0; j < tabuleiro[i].length; j++){
-        botao = document.createElement('button');
-        botao.setAttribute('type', 'button');
-        botao.setAttribute('id', 'bt' + i + "" + j);
-        botao.setAttribute('class' , 'btJogo' + i);
-        botao.setAttribute('onclick', 'marca(' + i + "," + j + ')');
-        botao.append(document.createTextNode(""));
+for (let i = 0; i < tabuleiro.length; i++) {
+    quebralinha = document.createElement('br')
+    document.body.append(quebralinha);
+    for (let j = 0; j < tabuleiro[i].length; j++) {
+        botao = document.createElement('button')
+        botao.setAttribute('type', 'button')
+        botao.setAttribute('id', 'bt' + i + "" + j)
+        botao.setAttribute('class', 'btJogo' + i)
+        botao.setAttribute('onclick', 'marca(' + i + "," + j + ")")
+        botao.append(document.createTextNode(""))
         document.body.append(botao);
     }
 }
 
-function marca00(linha, coluna){
-    marcarCasa("bt" + linha + "" + coluna);
+function marca(linha, coluna) {
+    marcarCasa("bt" + linha + "" + coluna)
+
 }
 
 
-function marcarCasa(nomeBotao){
-    if(jogada % 2 === 0){
-        document.getElementById(nomeBotao).innerText = "O";
+function marcarCasa(nomeBotao) {
+    if (jogada % 2 == 0) {
+        document.getElementById(nomeBotao).innerText = "O"
+        document.getElementById(nomeBotao).style.color = "blue";
     } else {
-        document.getElementById(nomeBotao).innerText = "X";
+        document.getElementById(nomeBotao).innerText = "X"
+        document.getElementById(nomeBotao).style.color = "black";
     }
-    document.getElementById(nomeBotao).disabled = true;
-    jogada++;
+    document.getElementById(nomeBotao).disabled = true
+    jogada++
+    if (jogada >= 5) {
+        encerraJogo()
+    }
+}
+
+function encerraJogo() {
+    
+        if (document.getElementById("bt00").innerText ==
+        document.getElementById("bt01").innerText &&
+        document.getElementById("bt01").innerText ==
+        document.getElementById("bt02").innerText &&
+        document.getElementById("bt00").innerText != "") {
+        alert("Jogo Finalizado\n Vencedor: " + document.getElementById("bt00").innerText)
+        for(cont in tabuleiro){
+            document.getElementById("bt0" + cont).style.color = "#FF0000"
+        }
+    }
+    if (document.getElementById("bt10").innerText ==
+        document.getElementById("bt11").innerText &&
+        document.getElementById("bt11").innerText ==
+        document.getElementById("bt12").innerText &&
+        document.getElementById("bt10").innerText != "") {
+        alert("Jogo Finalizado\n Vencedor: " + document.getElementById("bt10").innerText)
+        for(cont in tabuleiro){
+            document.getElementById("bt1" + cont).style.color = "#FF0000"
+        }
+    }
+    if (document.getElementById("bt20").innerText ==
+        document.getElementById("bt21").innerText &&
+        document.getElementById("bt21").innerText ==
+        document.getElementById("bt22").innerText &&
+        document.getElementById("bt20").innerText != "") {
+        alert("Jogo Finalizado\n Vencedor: " + document.getElementById("bt20").innerText)
+        for(cont in tabuleiro){
+            document.getElementById("bt2" + cont).style.color = "#FF0000"
+        }
+    }
+    if (document.getElementById("bt00").innerText ==
+        document.getElementById("bt10").innerText &&
+        document.getElementById("bt10").innerText ==
+        document.getElementById("bt20").innerText &&
+        document.getElementById("bt00").innerText != "") {
+        alert("Jogo Finalizado\n Vencedor: " + document.getElementById("bt00").innerText)
+        for(cont in tabuleiro){
+            document.getElementById("bt" + cont + "0").style.color = "#FF0000"
+        }
+    }
+    if (document.getElementById("bt01").innerText ==
+        document.getElementById("bt11").innerText &&
+        document.getElementById("bt11").innerText ==
+        document.getElementById("bt21").innerText &&
+        document.getElementById("bt01").innerText != "") {
+        alert("Jogo Finalizado\n Vencedor: " + document.getElementById("bt01").innerText)
+        for(cont in tabuleiro){
+            document.getElementById("bt" + cont + "1").style.color = "#FF0000"
+        }
+    }
+    if (document.getElementById("bt02").innerText ==
+        document.getElementById("bt12").innerText &&
+        document.getElementById("bt12").innerText ==
+        document.getElementById("bt22").innerText &&
+        document.getElementById("bt02").innerText != "") {
+        alert("Jogo Finalizado\n Vencedor: " + document.getElementById("bt02").innerText)
+        for(cont in tabuleiro){
+            document.getElementById("bt" + cont + "2").style.color = "#FF0000"
+        }
+    }
+    if (document.getElementById("bt00").innerText ==
+        document.getElementById("bt11").innerText &&
+        document.getElementById("bt11").innerText ==
+        document.getElementById("bt22").innerText &&
+        document.getElementById("bt00").innerText != "") {
+        alert("Jogo Finalizado\n Vencedor: " + document.getElementById("bt00").innerText)
+        for(cont in tabuleiro){
+            document.getElementById("bt" + cont + ""+ cont).style.color = "#FF0000"
+        }
+    }
+    if (document.getElementById("bt02").innerText ==
+        document.getElementById("bt11").innerText &&
+        document.getElementById("bt11").innerText ==
+        document.getElementById("bt20").innerText &&
+        document.getElementById("bt02").innerText != "") {
+        alert("Jogo Finalizado\n Vencedor: " + document.getElementById("bt02").innerText)
+        let contRegressiva = tabuleiro.length -1
+        for(cont in tabuleiro){
+            document.getElementById("bt" + cont + "" + contRegressiva--).style.color = "#FF0000"
+        }
+    }
 }
