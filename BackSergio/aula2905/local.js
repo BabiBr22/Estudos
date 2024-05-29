8a02297474161b21d1fee6e0f739199bb3231780
BÁRBARA COELHO RIOS <barbara_rios@estudante.sesisenai.org.br>
2024-05-29 17:16:15 -0300

    Comit do e-commerce feito em aula com professor

---
+++ BackSergio/aula2905/local.js
@@ -0,0 +1,20 @@
+
+//criar os elementos
+
+
+// localStorage.setItem('nome', 'Bárbara')
+// localStorage.setItem('idade', '20')
+// localStorage.setItem('altura', '1.60')
+
+//----------------------
+//pegar os elementos
+
+// let nome = localStorage.getItem('nome')
+// let idade = localStorage.getItem('idade')
+// let altura = localStorage.getItem('altura')
+// alert(nome + " - " + idade + " - " + altura)
+
+//-----------------------
+//remover os itens
+
+localStorage.removeItem('altura')
