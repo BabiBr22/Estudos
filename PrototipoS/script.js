3d48f57deb53368ac00fc8d18b5c04f7cc0d2da3
BÁRBARA COELHO RIOS <barbara_rios@estudante.sesisenai.org.br>
2024-06-12 17:10:37 -0300

    Commit das coisas da aula de hoje sobre o site e-comerce

--- PrototipoS/script.js
+++ PrototipoS/script.js
@@ -1,4 +1,4 @@
-function obterValor(){
-    let botao = document.getElementById("bt").innerHTML
-    alert("O valor do botão é: " + botao);
+function obterValor(botao) {
+    let valor = botao.innerHTML;
+    alert("O valor do botão é: " + valor);
 }
