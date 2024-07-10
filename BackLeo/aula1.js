062aa8787949c5d90ea1517c46eae963997ea0e8
BÁRBARA COELHO RIOS <barbara_rios@estudante.sesisenai.org.br>
2024-07-10 17:18:18 -0300

    Comiti da aula de hoje

---
+++ BackLeo/aula1.js
@@ -0,0 +1,21 @@
+// let nomes = ["barbara", "bruna", "beatriz", "roberto", "caio", "diego", "diogo", "mario"]
+
+// console.log(nomes.sort())
+
+
+// ///////////////////////////////////////////////////////////////////////////// //
+
+// const pacoteInstalados = ["react", "lodash", "axios", "express"];
+
+// console.log(pacoteInstalados.includes("loadash"))
+// console.log(pacoteInstalados.includes("react"))
+// console.log(pacoteInstalados.includes("axios"))
+// console.log(pacoteInstalados.includes("express"))
+
+
+// ///////////////////////////////////////////////////////////////////////////// //
+const nomes = ["Ana" , "Bruna", "Carla, Daniel, Elema, Francisco"];
+
+function busca_nomes(nomes){
+    console.log(nomes.indexOf(nomes))
+}
