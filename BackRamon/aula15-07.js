7e297b284c3dc86d8c95e185cabaef90be734465
BÁRBARA COELHO RIOS <barbara_rios@estudante.sesisenai.org.br>
2024-07-15 17:18:43 -0300

    comit restante

--- BackRamon/aula15-07.js
+++ BackRamon/aula15-07.js
@@ -1,9 +1,32 @@
 
 var pessoa = {nome:'Bárbara', idade: 20, cpf:2919303213 , email:'barbara@gmail.com',hobbies:['jogar', 'ver Alanzoka', 'dormir'], saudacao:function(){
-    return 'Meu nome é ' + this.nome + '.'}
+    return 'Meu nome é ' + this.nome + '.'}, 
+    endereco: 
+    {rua:'rua do senai',
+    bairro:'distrito industrial', 
+    cidade:'São josé'}
 }
 
 
 
 console.log(pessoa.saudacao())
 console.log(pessoa.hobbies[0])
+console.log(pessoa.endereco.rua)
+
+var carro = {nome: 'doblo', portas: 4, rodas:null, ano: 2010, funcionando: true, donos:['patricia', 'joao', 'rodolfo'], pecas:{pneu: 'gag',
+motor: 'g2', cor: 'bordo'}, fabricante: function(){
+    return 'Esse carro foi criado na Italia'
+},
+
+}
+
+carro.nome = 'uno'
+
+carro.rg = 7557675
+delete carro.portas
+console.log(carro)
+console.log(carro.rg)
+
+
+
+
